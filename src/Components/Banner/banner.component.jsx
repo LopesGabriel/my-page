fba7e54d36d08ee5e4febcc2568ed0fc1efa8387
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-06-21 00:30:58 -0300

    First Commit

---
+++ src/Components/Banner/banner.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Gabriel from '../../assets/img/Gabriel.png';
+import './banner.style.css';
+
+const Banner = (props) => {
+    return(
+        <Container className="banner-root" fluid>
+            <Row>
+                <Col className="banner-content">
+                    <div className="banner-image-container">
+                        <Image src={Gabriel} className="banner-image shadow" roundedCircle />
+                    </div>
+                    <h1 className="banner-title">Gabriel de Oliveira Lopes</h1>
+                    <h4 className="banner-subtitle">Desenvolver é arte e eu faço parte</h4>
+                    <div style={{marginTop: 20}}>
+                        <div className="banner-social-media-container">
+                            <a href="https://www.linkedin.com/in/gabriel-de-oliveira-lopes-b97093179/" target="_blank">
+                                <FontAwesomeIcon icon={faLinkedin} size="2x" style={{color: '#0e76a8'}} />
+                            </a>
+                            <a href="https://github.com/LopesGabriel" target="_blank" className="social-link">
+                                <FontAwesomeIcon icon={faGithub} size="2x" style={{color: 'black'}} />
+                            </a>
+                            <a href="https://www.instagram.com/gabriel.310/" target="_blank" title="@gabriel.310" className="social-link">
+                                <FontAwesomeIcon icon={faInstagram} size="2x" className="insta-icon" style={{color: 'white'}} />
+                            </a>
+                            <a href="https://www.facebook.com/gabriel.oliveiralopes.98" target="_blank" className="social-link">
+                                <FontAwesomeIcon icon={faFacebook} size="2x" style={{color: '#4267B2'}} />
+                            </a>
+                        </div>
+                        <p style={{margin: 0}}>Onde me encontrar</p>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Banner;
---
+++ src/Components/Header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './header.style.css';
+
+const Header = () => {
+    return(
+        <Navbar bg="dark" expand="lg">
+            <Navbar.Brand style={{color: 'white', fontWeight: 'bold'}} href="#home">Dev Lopes</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link style={{color: 'white'}} href="#home">Home</Nav.Link>
+                    <Nav.Link style={{color: 'white'}} href="#repositorio">Repositório</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
