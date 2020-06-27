5d3612d9273439efe82650f5b312398d073f9f43
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-06-27 17:12:26 -0300

    Chamando function para criar documento de contato

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-// import Header from './Components/Header/header.component';
 import Banner from './Components/Banner/banner.component';
 import About from './Components/About/about.component';
 import Contato from './Components/Contato/contato.component';
-import firebase from './firebase';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Header /> */}
       <Banner />
       <About />
       <Contato onSubmit={contactFormSubmit} />
@@ -28,43 +25,25 @@ function App() {
  */
 const contactFormSubmit = async (nome, email, mensagem, telefone = null) => {
 
-  if(nome.lenght < 3) {
-    alert("Nome deve ter mais de 3 caracteres.");
-    return false;
-  } else if(email.lenght < 10) {
-    alert("Email deve conter no mínimo 5 caracteres.");
-    return false;
-  } else if(mensagem.lenght < 10) {
-    alert("A mensagem deve conter no mínimo 10 caracteres.");
-    return false;
+  const dados = {
+    nome,
+    email,
+    mensagem,
+    telefone
   }
 
-  console.log(`Nome length: ${nome.lenght}, Email lenght: ${email.lenght}, Mensagem lenght: ${mensagem.lenght}`);
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dados)
+  }
 
-  return await sendContactData({
-    nome, email, mensagem, telefone
-  }).then(rs => {
-    console.info("Mensagem enviada com sucesso!", rs);
-    alert("Mensagem enviada!");
-    return rs;
-  })
-    .catch(err => {
-      console.error(err);
-      return alert("Não foi possível enviar a mensagem!");
-    });
+  const response = fetch(' https://us-central1-gabriel-homolog.cloudfunctions.net/gabrielContato', requestOptions);
+
+  const data = await response.json();
+  console.info("Dados retornados =>", data);
+  alert(data.message);
+
 }
-
-const sendContactData = dados => new Promise((resolve, reject) => {
-  firebase.firestore().collection('contatos').add({
-    nome: dados.nome,
-    email: dados.email,
-    mensagem: dados.mensagem,
-    telefone: dados.telefone
-  }).then(docRef => resolve(docRef.id))
-    .catch(err => {
-      console.error('Erro ao criar novo documento =>', err);
-      return reject(err);
-    });
-});
 
 export default App;
